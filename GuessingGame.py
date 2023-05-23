98a3a954d5e6900b78b0dd56fbdd213011bf8858
Anele <ngcobo.anele@gmail.com>
2023-05-23 17:13:14 +0200

    Initialising input game

---
+++ GuessingGame.py
@@ -0,0 +1,8 @@
+print ("Guess a number between 0-10")
+x= int(input("Enter number: "))
+
+if x== 7 or x== 3:
+    print("You have won the lotto!!!")
+else:
+    print("Kwannk, kwank, kwaaaaaank-sorry, you loose!")
+    
